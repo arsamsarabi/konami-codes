08561a737d0a64e779d678fdf735b2ddea507bef
Arsam Sarabi <arsamsarabi@me.com>
2024-05-28 09:14:02 +0100

    WIP KCodes in app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,38 @@
-import { Konami } from "@arsams/konami-codes";
+"use client";
+
+import { KonamiCodes } from "@arsams/konami-codes";
 import styles from "./page.module.css";
+import { useEffect } from "react";
 
 export default function Page(): JSX.Element {
-  const konami = new Konami();
+  const code = ["ArrowUp", "ArrowUp", "ArrowUp"];
+
+  const callback = () => console.log("hello konami codes!");
+
+  const konami = new KonamiCodes(
+    [
+      {
+        code: code,
+        cb: callback,
+      },
+    ],
+    {
+      delay: 10000,
+    }
+  );
+
+  const handleKeyDown = ({ key }: KeyboardEvent) => {
+    console.log(key);
+    konami.input(key);
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return <main className={styles.main}>Hello</main>;
 }
